0cae75b780dc27c970bdd3cf1b0501d9ec23f6d2
alexistobar <tobaralexis.89@mail.com>
2024-05-15 11:40:01 -0400

    pruebas del zoom

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,21 @@
-//crea elemento
+// Crear elemento video
 const video = document.createElement("video");
 
-//nuestro camvas
+// Nuestro canvas
 const canvasElement = document.getElementById("qr-canvas");
 const canvas = canvasElement.getContext("2d");
 
-//div donde llegara nuestro canvas
+// Div donde llegará nuestro canvas
 const btnScanQR = document.getElementById("btn-scan-qr");
 
-//lectura desactivada
+// Lectura desactivada
 let scanning = false;
 
-//funcion para encender la camara
+// Inicializar variables de zoom
+let zoom = 1; // Zoom inicial
+const zoomIncrement = 0.1; // Incremento de zoom
+
+// Función para encender la cámara con zoom
 const encenderCamara = () => {
   navigator.mediaDevices
     .getUserMedia({ video: { facingMode: "environment" } })
@@ -27,7 +31,7 @@ const encenderCamara = () => {
     });
 };
 
-//funciones para levantar las funiones de encendido de la camara
+// Funciones para levantar las funciones de encendido de la cámara
 function tick() {
   canvasElement.height = video.videoHeight;
   canvasElement.width = video.videoWidth;
@@ -44,7 +48,27 @@ function scan() {
   }
 }
 
-//apagara la camara
+// Función para aplicar zoom a la cámara
+const aplicarZoom = (factor) => {
+  const track = video.srcObject.getVideoTracks()[0];
+  const capabilities = track.getCapabilities();
+  if (capabilities.zoom) {
+    zoom = Math.max(capabilities.zoom.min, Math.min(capabilities.zoom.max, zoom + factor));
+    track.applyConstraints({ advanced: [{ zoom: zoom }] });
+  }
+};
+
+// Función para reducir el zoom
+const reducirZoom = () => {
+  aplicarZoom(-1 * zoomIncrement); // Invertimos el incremento para reducir el zoom
+};
+
+// Función para aumentar el zoom
+const aumentarZoom = () => {
+  aplicarZoom(zoomIncrement);
+};
+
+// Apagar la cámara
 const cerrarCamara = () => {
   video.srcObject.getTracks().forEach((track) => {
     track.stop();
@@ -58,29 +82,20 @@ const activarSonido = () => {
   audio.play();
 }
 
-//callback cuando termina de leer el codigo QR
+// Callback cuando termina de leer el código QR
 qrcode.callback = (respuesta) => {
   if (respuesta) {
-    //console.log(respuesta);
     Swal.fire(respuesta)
     activarSonido();
-    //encenderCamara();    
-    cerrarCamara();    
-
+    cerrarCamara();
   }
 };
-//evento para mostrar la camara sin el boton 
+
+// Evento para mostrar la cámara sin el botón 
 window.addEventListener('load', (e) => {
   encenderCamara();
-})
+});
 
-
-
-  
-      
-  
-
-
-
-
-
+// Event listeners para el zoom
+document.getElementById('zoomIn').addEventListener('click', aumentarZoom);
+document.getElementById('zoomOut').addEventListener('click', reducirZoom);
